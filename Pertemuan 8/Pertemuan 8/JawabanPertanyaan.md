c996b2faa3f32a7a90c9320cc50e45d7a4ed6ec9
Nada Alya <56966811+nadaalyak@users.noreply.github.com>
2020-04-01 23:56:27 +0700

    Add files via upload

---
+++ Pertemuan 8/Pertemuan 8/JawabanPertanyaan.md
@@ -0,0 +1,41 @@
+# Jawaban Pertanyaan Praktikum Stack (Jobsheet 8)
+
+## Jawaban Pertanyaan 8.3.3
+
+1.
+<img src = "nomor 1.png">
+
+Angka 4 dalam kurung untuk mengisi jumlah data yang akan dibuat
+
+2.
+
+<img src = code.png> <img src = output.png>
+
+3.
+Karena isi data maksimal adalah 4.yang dimasukkan adalah 17 terlebih dahulu,sehingga 98 muncul sebagai isi stack penuh
+
+
+4.
+
+
+5.
+Memastikan nilai top itu sendiri berada pada indeks 0
+
+
+## Jawaban Pertanyaan 8.4.3
+
+1.
+Untuk menentukan derajat masing-masing operator.
+Urutan operatornya adalah () , ^ , * / % memiliki derajat yang sama dilihat dari tanda mana yang paling depan , + - memiliki derajat yang sama dilihat dari tanda mana yang paling depan
+
+
+2.
+
+<img src = "nomor 2.png">
+
+3.
+ Jika ada tanda ‘(‘ maka di push ke stack sampai bertemu tanda ‘)’ maka pop isi stack sampai ditemukan tanda ‘(‘ kemudian ke postfix, sedangkan tanda ‘(‘ tidak dimasukkan ke postfix
+
+ 4.
+ 
+ <img src = "nomor 4.png">
---
+++ Pertemuan 8/Pertemuan 8/src/NomorDua/Stack.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NomorDua;
+
+/**
+ *
+ * @author Nada Alya
+ */
+public class Stack {
+    public int size;
+    public int top;
+    public String kalimat = "";
+    public String kata[] = new String[10];
+    
+    public Stack(String kalimat){
+    this.kalimat = kalimat;
+    size = kalimat.length();
+    top = -1;
+    }
+    public void push(String temp){
+    top++;
+    kata[top] = temp;
+    }
+    public String pop(int top){
+    String temp = kata[top];
+    top--;
+    return temp;
+    }
+    public void exchange() {
+        String temp = "";
+        for (int i = 0; i < size; i++) {
+            temp += String.valueOf(kalimat.charAt(i));
+            if(kalimat.charAt(i)==' '){
+                push(temp);
+                temp = "";
+            }
+        }
+        push(temp);
+    }
+    public void show() {
+        String temp = "";
+
+        for (int i = top; i >= 0; i--) {
+            temp += pop(i) + " ";
+        }
+        System.out.println("Kalimat dibalik : " + temp);
+    }
+}
